3ac5b923d636da9f74a3ce354c3e476b7f20bd20
nhan1206 <85447657+nhan1206@users.noreply.github.com>
2021-09-17 14:22:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phảichăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Thick anh nhiều lắm <33',
     btnNo: 'Không, Anh à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: ' nhắn ins cho anh  <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Trung thi đi chơi với anh qua đón nhé công chúa.',
+    messDesc: 'Trung thi đi chơi với anh qua đón nhé em bé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com/nhanguyen1080'
 }
